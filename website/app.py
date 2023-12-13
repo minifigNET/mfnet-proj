efd43533c2847836cce651c199fbcd3d8776f8c8
Gillian Law <gillianalaw@gmail.com>
2023-12-13 17:01:18 +0100

    Lego colours looking ugly

--- website/app.py
+++ website/app.py
@@ -1,9 +1,11 @@
 #NOTE TO TEAM - TO OPEN THE FILE ONLINE: streamlit run app.py
 #or now: https://minifigurenet.streamlit.app
 
+#Lego colours: https://usercontent.flodesk.com/de8064d5-0183-4c90-a0f9-0da438a65bce/upload/37cfed05-9017-4e5b-b5b8-ae11a341f132.pdf
 
 import streamlit as st
 import base64
+import toml
 
 st.set_page_config(
             page_title="MinifigNET",
@@ -11,29 +13,80 @@ st.set_page_config(
             layout="centered", # wide
             initial_sidebar_state="auto") # collapsed
 
-#TODO add background image
-@st.cache(allow_output_mutation=True)
-def get_base64_of_bin_file(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
 
-def set_png_as_page_bg(png_file):
-    bin_str = get_base64_of_bin_file(png_file)
-    page_bg_img = '''
+
+#TODO add background image - I really don't know how to do this!
+
+
+
+#TRIAL ONE:
+# @st.cache_data(allow_output_mutation=True)
+# @st.cache_data()
+# def get_base64_of_bin_file(bin_file):
+#     with open(bin_file, 'rb') as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
+
+# def set_png_as_page_bg(png_file):
+#     bin_str = get_base64_of_bin_file(png_file)
+#     page_bg_img = (
+#     <style>
+#     body {
+    # background-image: url("website/blueblocks.png;base64,%s");
+#         background-image: url("website/blueblocks/png;base64,%s");
+
+#     background-size: cover;
+#     }
+#     </style>)
+#     % bin_str
+
+#     st.markdown(page_bg_img, unsafe_allow_html=True)
+#     return
+
+# set_png_as_page_bg('blueblocks.png')
+
+
+#TRIAL TWO:
+st.markdown(
+    """
     <style>
-    body {
-    background-image: url("website:image/jpg;base64,%s");
-    background-size: cover;
+    .reportview-container {
+        background: url("website/blueblocks.png");
     }
-    </style>
-    ''' % bin_str
+   </style>
+    """,
+    unsafe_allow_html=True
+)
 
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
+#TRIAL THREE:
 
-set_png_as_page_bg('blueblocks.png')
 
+with open('minifig.toml', 'r') as f:
+    config = toml.load(f)
+
+theme = config.get('theme', {})
+primaryColor = theme.get('primaryColor')
+backgroundColor = theme.get('backgroundColor')
+secondaryBackgroundColor = theme.get('secondaryBackgroundColor')
+textColor = theme.get('textColor')
+font = theme.get('font')
+
+# Create a CSS string using your theme settings
+css = f"""
+<style>
+.stMarkdown {{
+    background-color: {secondaryBackgroundColor};
+    color: {textColor};
+    font-family: {font};
+}}
+</style>
+"""
+
+# Apply the CSS to your Streamlit app
+st.markdown(css, unsafe_allow_html=True)
+
+# Now when you use st.markdown, it will be styled with your theme settings
+st.markdown("Experimenting with themes - bear with me...")
 
 #TODO Link HERE to database and search function
 
