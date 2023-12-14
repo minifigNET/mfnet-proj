0fa5e5614895d14fde55a92cd15e398fb41535a5
Gillian Law <gillianalaw@gmail.com>
2023-12-14 17:56:18 +0100

    dropdown started

--- website/app.py
+++ website/app.py
@@ -141,8 +141,79 @@ st.text("")
 st.title('Can you help us to learn more?')
 st.write("Do you know the name of a minifig that we don't have on your system? If so, please let us know!")
 
-user_text = st.text_input('Name:')
-st.write(f'You entered: {user_text}')
+# user_text = st.text_input('Name:')
+# st.write(f'You entered: {user_text}')
+st.text("")
+st.text("")
+
+#based on https://en.wikipedia.org/wiki/Lego_Minifigures_(theme)
+option = st.selectbox(
+    'What class does the minifig belong to?',
+    ('No: 43 - Marvel Collectible Minifigure Series 1 (Set Number: 71039)',
+     'No: 42 - Disney 100 Minifigure Series (Set Number: 71038)',
+     'No: 41 - Series 24 (Set Number: 71037)',
+     'No: 40 - Series 23 (Set Number: 71034)',
+     'No: 39 - Muppets Series (Set Number: 71033)',
+     'No: 38 - Series 22 (Set Number: 71032)',
+     'No: 37 - Marvel Collectible Minifigure Series 2 (Set Number: 71031)',
+     'No: 36 - Looney Tunes Series (Set Number: 71030)',
+     'No: 35 - Series 21 (Set Number: 71029)',
+     'No: 34 - Harry Potter Series 2 (Set Number: 71028)'
+
+
+
+
+
+
+
+     ))
+
+st.write('You selected Class:', option)
+
+if option == 'No: 43 - Marvel Collectible Minifigure Series 1 (Set Number: 71039)':
+    marvel1_option = st.selectbox('Select character name', ('Minifig 1', 'Minifig 2'))
+    st.write('You selected Marvel Collectible Minifigure Series 1:', marvel1_option)
+
+elif option == 'No: 42 - Disney 100 Minifigure Series (Set Number: 71038)':
+    disney_option = st.selectbox('Select character name', ('Minifig 1', 'Minifig 2'))
+    st.write('You selected Disney 100 Minifigure Series:', disney_option)
+
+elif option == 'No: 41 - Series 24 (Set Number: 71037)':
+    series24_option = st.selectbox('Select character name', ('Minifig 1', 'Minifig 2'))
+    st.write('You selected Series 24:', series24_option)
+
+elif option == 'No: 40 - Series 23 (Set Number: 71034)':
+    series23_option = st.selectbox('Select character name', ('Minifig 1', 'Minifig 2'))
+    st.write('You selected Series 23:', series23_option)
+
+elif option == 'No: 39 - Muppets Series (Set Number: 71033)':
+    muppets_option = st.selectbox('Select character name', ('Animal', 'Swedish Chef', 'Miss Piggy', 'Kermit'))
+    st.write('You selected Muppets Series:', muppets_option)
+
+elif option == 'No: 38 - Series 22 (Set Number: 71032)':
+    series22_option = st.selectbox('Select character name', ('Minifig 1', 'Minifig 2'))
+    st.write('You selected Series 22:', series22_option)
+
+elif option == 'No: 37 - Marvel Collectible Minifigure Series 2 (Set Number: 71031)':
+    marvel2_option = st.selectbox('Select character name', ('Minifig 3', 'Minifig 4'))
+    st.write('You selected Marvel Collectible Minifigure Series 2:', marvel2_option)
+
+elif option == 'No: 36 - Looney Tunes Series (Set Number: 71030)':
+    looney_option = st.selectbox('Select character name', ('Minifig 1', 'Minifig 2'))
+    st.write('You selected Looney Tunes Series:', looney_option)
+
+elif option == 'No: 35 - Series 21 (Set Number: 71029)':
+    series21_option = st.selectbox('Select character name', ('Minifig 1', 'Minifig 2'))
+    st.write('You selected Disney 100 Minifigure Series:', series21_option)
+
+elif option == 'No: 34 - Harry Potter Series 2 (Set Number: 71028)':
+    harry_option = st.selectbox('Select character name', ('Harry', 'Hermione', 'Ron'))
+    st.write('You selected Harry Potter Series 2:', harry_option)
+
+
+
+
+
 
 #TODO add drop dwn with all classes and names - api response , hard code to start
 
